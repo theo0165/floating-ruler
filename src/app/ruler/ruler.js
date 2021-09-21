07bf12c26e99a4a20205ba4cfd925eb09941b2be
Theo Sandell <theo01sandell@gmail.com>
2021-09-21 22:39:42 +0200

    Created basic ruler funtion. Added font

--- src/app/ruler/ruler.js
+++ src/app/ruler/ruler.js
@@ -1,5 +1,21 @@
-let closeBtn = document.querySelector("#close_btn");
+let bottomLine = document.querySelector("#width");
+let rightLine = document.querySelector("#height");
 
-closeBtn.addEventListener("click", function() {
-    console.log("close");
+let heightInfo = document.querySelector("#height_info");
+let widthInfo = document.querySelector("#width_info");
+
+function setSizeInfo(){
+    let height = rightLine.offsetHeight - 82;
+    let width = bottomLine.offsetWidth - 82;
+
+    heightInfo.innerText = height + "px";
+    widthInfo.innerText = width + "px";
+}
+
+window.addEventListener("resize", function() {
+    setSizeInfo();
 })
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    setSizeInfo();
+});
